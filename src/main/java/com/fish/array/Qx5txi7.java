f93ea4ae5a783144cb697b623924bcb2c236366a
liuqi <liu-qi@bupt.edu.cn>
2023-07-05 15:55:38 +0800

    feat(code): 20230705(补充快速选择算法实现Qx5txi7)

--- src/main/java/com/fish/array/Qx5txi7.java
+++ src/main/java/com/fish/array/Qx5txi7.java
@@ -2,7 +2,9 @@ package com.fish.array;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
 
 /**
  * https://leetcode.cn/leetbook/read/tencent/x5txi7/
@@ -13,7 +15,20 @@ import java.util.*;
 public class Qx5txi7 {
     // 自己的方法，优先队列实现，时间复杂度为nlogn级别
     class Solution1 {
+        // 小顶堆，维护整个数组最大的k个元素，遍历完成后，堆顶元素就是答案。可以降低时间和空间复杂度
         public int findKthLargest(int[] nums, int k) {
+            Queue<Integer> q = new PriorityQueue<Integer>((a, b) -> b - a);
+            for (int num : nums) {
+                q.add(num);
+                if (q.size() > k) {
+                    q.poll();
+                }
+            }
+            return q.poll();
+        }
+
+        // 大顶堆，先将所有元素插入堆，然后删除k-1次，此时堆顶元素就是答案
+        public int findKthLargest1(int[] nums, int k) {
             Queue<Integer> q = new PriorityQueue<Integer>((a, b) -> b - a);
             for (int num : nums) {
                 q.add(num);
@@ -25,22 +40,75 @@ public class Qx5txi7 {
         }
     }
 
-    // todo 快排
+    //  todo 手动实现优先队列
     class Solution2 {
         public int findKthLargest(int[] nums, int k) {
             return -2;
         }
     }
 
-    //  todo 手动实现优先队列
+    //  快速选择算法
     class Solution3 {
+        Random random = new Random();
+
+        public int quickSelect(int l, int r, int k, int[] nums) {
+            // 判断l和r是否重合，如果是，直接返回
+            if (l == r) {
+                return nums[l];
+            }
+            // 随机选择轴
+            int pivotIdx = Math.abs(random.nextInt()) % (r - l + 1) + l;
+            int pivot = nums[pivotIdx];
+            // 将小于pivot的数移动到左边，大于pivot的数移动到右边
+            nums[pivotIdx] = nums[r];
+            int lIdx = l, rIdx = r;
+            while (true) {
+                // 从左边找大于pivot的数，移动到右边
+                while (lIdx < rIdx && nums[lIdx] <= pivot) {
+                    lIdx++;
+                }
+                if (lIdx == rIdx) {
+                    if (rIdx == nums.length - k) {
+                        return pivot;
+                    } else if (rIdx < nums.length - k) {
+                        return quickSelect(rIdx + 1, r, k, nums);
+                    } else {
+                        return quickSelect(l, rIdx - 1, k, nums);
+                    }
+                }
+                swap(lIdx, rIdx--, nums);
+                // 从右边找小于pivot的数，移动到左边
+                while (rIdx > lIdx && nums[rIdx] >= pivot) {
+                    rIdx--;
+                }
+                if (rIdx == lIdx) {
+                    if (rIdx == nums.length - k) {
+                        return pivot;
+                    } else if (rIdx < nums.length - k) {
+                        return quickSelect(rIdx + 1, r, k, nums);
+                    } else {
+                        return quickSelect(l, rIdx - 1, k, nums);
+                    }
+                }
+                swap(lIdx++, rIdx, nums);
+            }
+        }
+
+        // 辅助函数
+        private void swap(int idx1, int idx2, int[] nums) {
+            int temp = nums[idx1];
+            nums[idx1] = nums[idx2];
+            nums[idx2] = temp;
+        }
+
         public int findKthLargest(int[] nums, int k) {
-            return 0;
+            return quickSelect(0, nums.length - 1, k, nums);
         }
     }
 
     @Test
     public void test() {
-        System.out.println("111");
+        int result = new Solution3().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2);
+        System.out.println(result);
     }
 }
