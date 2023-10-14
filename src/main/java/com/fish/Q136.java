a4f0e7dd54fcaf91749a7d2b47f278fa81f8ddc6
liuqi <liu-qi@bupt.edu.cn>
2023-10-14 19:20:10 +0800

    feat: 20231014 sad~~~

---
+++ src/main/java/com/fish/Q136.java
@@ -0,0 +1,22 @@
+package com.fish;
+
+/**
+ * @author liuqi
+ * @date 2023/10/14
+ *
+ * https://leetcode.cn/problems/single-number/?envType=daily-question&envId=2023-10-14
+ */
+public class Q136 {
+    /**
+     * 异或，想不出来
+     */
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int result = 0;
+            for (int num : nums) {
+                result = result ^ num;
+            }
+            return result;
+        }
+    }
+}
