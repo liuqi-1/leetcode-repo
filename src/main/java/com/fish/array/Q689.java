df94ad7de34b164c759fb545d0ebfad6f5b4dc80
liuqi <liu-qi@bupt.edu.cn>
2023-11-20 10:55:55 +0800

    feat(nope): 20231120

--- src/main/java/com/fish/array/Q689.java
+++ src/main/java/com/fish/array/Q689.java
@@ -11,9 +11,53 @@ import java.util.Arrays;
  */
 public class Q689 {
     /**
-     * 超时了 baka todo
+     * 不愧是官方题解
      */
     class Solution {
+        public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+            int[] ans = new int[3];
+            int sum1 = 0, maxSum1 = 0, maxSum1Idx = 0;
+            int sum2 = 0, maxSum12 = 0, maxSum12Idx1 = 0, maxSum12Idx2 = 0;
+            int sum3 = 0, maxTotal = 0;
+            for (int i = k * 2; i < nums.length; ++i) {
+                sum1 += nums[i - k * 2];
+                sum2 += nums[i - k];
+                sum3 += nums[i];
+                if (i >= k * 3 - 1) {
+                    if (sum1 > maxSum1) {
+                        maxSum1 = sum1;
+                        maxSum1Idx = i - k * 3 + 1;
+                    }
+                    if (maxSum1 + sum2 > maxSum12) {
+                        maxSum12 = maxSum1 + sum2;
+                        maxSum12Idx1 = maxSum1Idx;
+                        maxSum12Idx2 = i - k * 2 + 1;
+                    }
+                    if (maxSum12 + sum3 > maxTotal) {
+                        maxTotal = maxSum12 + sum3;
+                        ans[0] = maxSum12Idx1;
+                        ans[1] = maxSum12Idx2;
+                        ans[2] = i - k + 1;
+                    }
+                    sum1 -= nums[i - k * 3 + 1];
+                    sum2 -= nums[i - k * 2 + 1];
+                    sum3 -= nums[i - k + 1];
+                }
+            }
+            return ans;
+        }
+    }
+
+    // 作者：力扣官方题解
+    // 链接：https://leetcode.cn/problems/maximum-sum-of-3-non-overlapping-subarrays/
+    // 来源：力扣（LeetCode）
+    // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+    /**
+     * 超时了 baka todo
+     */
+    class Solution1 {
         int[] subSum;
 
         public int[] findMax(int[] nums, int sPos, int k, int cnt) {
